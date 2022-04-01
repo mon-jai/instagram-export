821a6d9fbdbc67b6e27e3c868ed7c2bc93252b84
mon-jai <91261297+mon-jai@users.noreply.github.com>
2022-04-02 01:45:57 +0800

    refactor: import command name from package.json

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
-export const COMMAND_NAME = "instagram-export"
+import { createRequire } from "module"
+
+export const { name: COMMAND_NAME } = createRequire(import.meta.url)("../package.json") as { name: string }
+
 export const KEYTAR_SERVICE_NAME = COMMAND_NAME
