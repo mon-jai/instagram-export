8cd840dbc96c437a66f4d2cc66b5f14a2813fd7e
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-01-11 04:31:48 +0800

    chore: analyze unused exports

--- build.js
+++ build.js
@@ -2,6 +2,10 @@ import { exec as execCallback } from "child_process"
 import { readdir, rm } from "fs/promises"
 import { relative, resolve } from "path"
 
+import analyzeTsConfigExport from "ts-unused-exports"
+
+const { default: analyzeTsConfig } = analyzeTsConfigExport
+
 async function exec(command) {
   return new Promise((resolve, reject) =>
     execCallback(command, (error, stdout, stderr) => {
@@ -15,6 +19,23 @@ async function exec(command) {
 
 const binPath = "./node_modules/.bin/"
 const outputPath = resolve("bin")
+
+const unusedExports = analyzeTsConfig("./tsconfig.json", ["--ignoreFiles=bin"])
+const noOfModulesWithUnusedExports = Object.keys(unusedExports).length
+
+if (noOfModulesWithUnusedExports > 0) {
+  console.log(
+    Object.fromEntries(
+      Object.entries(unusedExports).map(([filePath, exportNameAndLocations]) => [
+        relative(resolve(), filePath),
+        exportNameAndLocations.map(({ exportName }) => exportName),
+      ])
+    )
+  )
+  throw `${noOfModulesWithUnusedExports} module${
+    noOfModulesWithUnusedExports > 1 ? "s" : ""
+  } with unused exports found.`
+}
 
 await rm(outputPath, { recursive: true, force: true })
 const prettierOutput = await exec(`${resolve(binPath, "prettier")} --write .`)
